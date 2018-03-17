f3b5b38636bab9fcfaf4500a64492b886a9fedab
JoeyTribbiani <vophanhongdung@gmail.com>
2018-03-17 09:35:45 +0700

    setup tableview

---
+++ Shop-Swag/Views/CategoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryTableViewCell.swift
+//  Shop-Swag
+//
+//  Created by Dũng Võ on 3/17/18.
+//  Copyright © 2018 Dũng Võ. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
